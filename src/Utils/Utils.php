d16ece7d24acb82205b9b9984ed1add51f075f69
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2022-07-29 09:53:32 +0100

    new

---
+++ src/Utils/Utils.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Utils;
+
+
+class Utils
+{
+
+  public static function getRootDir(): string
+  {
+    return __DIR__ . '/../../assets/logs.txt';
+  }
+
+
+}
--- tests/Command/UploadLogCommandTest.php
+++ tests/Command/UploadLogCommandTest.php
@@ -4,17 +4,28 @@ namespace App\Tests\Command;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use App\Utils\Utils;
 
 class UploadLogCommandTest extends KernelTestCase
 {
+
+  protected $containerBag;
+
+
+
   public function testExecute()
   {
+
+
     $kernel = self::bootKernel();
     $application = new Application($kernel);
     $command = $application->find('app:upload-data');
     $commandTester = new CommandTester($command);
+    $dir = Utils::getRootDir();
+
     $commandTester->execute([
-      'FilePath' => 'C:/logs.txt',
+      'FilePath' => $dir,
     ]);
     $commandTester->assertCommandIsSuccessful();
     $output = $commandTester->getDisplay();
